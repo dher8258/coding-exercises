eeaf33c621fb858c277443eab1e635bfbf85374c
dher8258 <danihergar@air-de-daniel.lan>
2020-06-09 19:31:06 +0100

    First implementation to find closest value in BST

---
+++ closest-value-bst.py
@@ -0,0 +1,43 @@
+# This file is initialized with a code version of this
+# question's sample test case. Feel free to add, edit,
+# or remove test cases in this file as you see fit!
+
+import unittest
+
+def findClosestValueInBst(tree, target):
+    return findClosestValueInBSTHelper(tree, target, float("inf"))
+
+def findClosestValueInBSTHelper(tree, target, closest):
+    currentNode = tree
+    while currentNode is not None:
+        if abs(target - closest) > abs(target - currentNode.value):
+            closest = currentNode.value
+        if target < currentNode.value:
+            currentNode = currentNode.left
+        elif target > currentNode.value:
+            currentNode = currentNode.right
+        else:
+            break
+        return closest
+
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class TestProgram(unittest.TestCase):
+    def test_case_1(self):
+        root = BST(10)
+        root.left = BST(5)
+        root.left.left = BST(2)
+        root.left.left.left = BST(1)
+        root.left.right = BST(5)
+        root.right = BST(15)
+        root.right.left = BST(13)
+        root.right.left.right = BST(14)
+        root.right.right = BST(22)
+        expected = 13
+        actual = findClosestValueInBst(root, 12)
+        self.assertEqual(expected, actual)
