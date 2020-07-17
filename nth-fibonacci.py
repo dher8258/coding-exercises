6a97644e65624d463688d0bac255534ec70ac320
Daniel HG <danihergar@gmail.com>
2020-07-17 20:21:17 +0200

    Correct case n = 1 and add unit test for it

--- nth-fibonacci.py
+++ nth-fibonacci.py
@@ -11,7 +11,7 @@ def getNthFib(n):
         lastTwo[1] = nextFib
         counter += 1
 
-    return lastTwo[1] if n > 1 else lastTwo[1] 
+    return lastTwo[1] if n > 1 else lastTwo[0]
 
 class TestProgram(unittest.TestCase):
     def test_case_1(self):
@@ -22,6 +22,9 @@ class TestProgram(unittest.TestCase):
 
     def test_case_3(self):
         self.assertEqual(getNthFib(7), 8)
+
+    def test_case_4(self):
+        self.assertEqual(getNthFib(1), 0)
 
 if __name__ == '__main__':
     unittest.main()
