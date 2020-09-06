aa59ff49372b9a88fb7f12030d66c6017f629ad9
Daniel HG <danihergar@gmail.com>
2020-09-06 19:57:00 +0100

    feat: add unit tests to validate BST algorithm

--- Medium/validate_bst.py
+++ Medium/validate_bst.py
@@ -20,7 +20,61 @@ def validateBstHelper(tree, minValue, maxValue):
 
 class TestProgram(unittest.TestCase):
     def test_case_1(self):
-        pass
+        root = BST(10)
+        root.left = BST(5)
+        root.left.left = BST(2)
+        root.left.left.left = BST(1)
+        root.left.right = BST(5)
+        root.right = BST(15)
+        root.right.left = BST(13)
+        root.right.left.right = BST(14)
+        root.right.right = BST(22)
+        self.assertEqual(validateBst(root), True)
+
+    def test_case_2(self):
+        root = BST(10)
+        root.left = BST(5)
+        root.right = BST(15)
+        root.right.left = None
+        root.right.right = BST(22)
+        root.right.right.left = None
+        root.right.right.right = None
+        root.left.left = BST(2)
+        root.left.right = BST(5)
+        root.left.right.left = BST(2)
+        root.left.right.right = BST(5)
+        root.left.right.right.left = None
+        root.left.right.right.right = None
+        root.left.left.left = BST(1)
+        root.left.left.right = None
+        root.left.left.left.left = BST(-5)
+        root.left.left.left.right = None
+        root.left.left.left.left.left = BST(-15)
+        root.left.left.left.left.right = BST(-5)
+        root.left.left.left.left.right.left = None
+        root.left.left.left.left.right.right = BST(-2)
+        root.left.left.left.left.right.right.left = None
+        root.left.left.left.left.right.right.right = BST(-1)
+        root.left.left.left.left.right.right.right.left = None
+        root.left.left.left.left.right.right.right.right = None
+        root.left.left.left.left.left.left = BST(-22)
+        root.left.left.left.left.left.right = None
+        root.left.left.left.left.left.left.left = None
+        root.left.left.left.left.left.left.right = None
+        self.assertEqual(validateBst(root), False)
+
+    def test_case_3(self):
+        root = BST(10)
+        root.left = BST(5)
+        root.right = BST(15)
+        root.right.left = None
+        root.right.right = None
+        root.left.left = None
+        root.left.right = BST(10)
+        root.left.right.left = None
+        root.left.right.right = None
+        self.assertEqual(validateBst(root), False)
+
 
 if __name__ == '__main__':
     unittest.main()
