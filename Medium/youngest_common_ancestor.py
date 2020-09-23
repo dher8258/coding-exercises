a10d426420746e91f14b7642df41a13457f76344
Daniel HG <danihergar@gmail.com>
2020-09-23 12:43:55 +0100

    feat: add problem description to youngest common ancestor algorithm

--- Medium/youngest_common_ancestor.py
+++ Medium/youngest_common_ancestor.py
@@ -1,5 +1,21 @@
 import unittest
 
+"""
+Given three inputs, all of which are instances of an AncestralTree class that have an
+ancestor property pointing to their youngest ancestor. The first input is the top ancestor
+in an ancestral tree (i.e., the only instance that has no ancestor -- its ancestor property
+points to None / Null), and the other two inputs are descendants in the ancestral tree.
+
+The function returns the youngest common ancestor to the two descendants.
+
+Note that a descendant is considered its own ancestor. So in the simple ancestral tree below,
+the youngest common ancestor to nodes A and B is node A.
+
+    A
+   /
+  B
+"""
+
 class AncestralTree:
     def __init__(self, name):
         self.name = name
