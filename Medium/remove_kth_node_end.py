e2067748249e3c7a18caf34bc2a4bc0fdb5a9d49
Daniel HG <danihergar@gmail.com>
2020-09-27 20:02:07 +0100

    chore: add description to the remove kth node from end algo

--- Medium/remove_kth_node_end.py
+++ Medium/remove_kth_node_end.py
@@ -1,5 +1,26 @@
 import unittest
 
+"""
+The function takes in the head of a Singly Linked List and an integer k
+and removes the kth node from the end of the list.
+
+The removal is done in place, meaning that the original data structure
+is mutated (no new structure is created).
+
+Furthermore, the input head of the linked list remains the head of the
+linked list after the removal is done, even if the head is the node that's
+supposed to be removed. In other words, if the head is the node that's
+supposed to be removed, the function simply mutate its value and next pointer.
+
+The function does not return anything.
+
+It is assumed that the Linked List always has at least two nodes and, more
+specifically, at least k nodes.
+
+Each LinkedList node has an integer value as well as a next node pointing to
+the next node in the list or to None / null if it's the tail of the list.
+"""
+
 class LinkedList:
     def __init__(self, value):
         self.value = value
