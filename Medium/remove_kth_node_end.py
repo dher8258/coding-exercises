28f243b4036a1eca30c4460f791c7e43f1765f5a
Daniel HG <danihergar@gmail.com>
2020-09-27 19:52:12 +0100

    feat: add unit tests to remove Kth node algo

--- Medium/remove_kth_node_end.py
+++ Medium/remove_kth_node_end.py
@@ -46,5 +46,59 @@ class TestProgram(unittest.TestCase):
         removeKthNodeFromEnd(test, 4)
         self.assertEqual(test.getNodesInArray(), expected.getNodesInArray())
 
+    def test_case_2(self):
+        test = LinkedList(0).addMany([1, 2, 3, 4, 5, 6, 7, 8, 9])
+        expected = LinkedList(0).addMany([1, 2, 3, 4, 5, 6, 7, 8])
+        removeKthNodeFromEnd(test, 1)
+        self.assertEqual(test.getNodesInArray(), expected.getNodesInArray())
+
+    def test_case_3(self):
+        test = LinkedList(0).addMany([1, 2, 3, 4, 5, 6, 7, 8, 9])
+        expected = LinkedList(0).addMany([1, 2, 3, 4, 5, 6, 7, 9])
+        removeKthNodeFromEnd(test, 2)
+        self.assertEqual(test.getNodesInArray(), expected.getNodesInArray())
+
+    def test_case_4(self):
+        test = LinkedList(0).addMany([1, 2, 3, 4, 5, 6, 7, 8, 9])
+        expected = LinkedList(0).addMany([1, 2, 3, 4, 5, 6, 8, 9])
+        removeKthNodeFromEnd(test, 3)
+        self.assertEqual(test.getNodesInArray(), expected.getNodesInArray())
+
+    def test_case_5(self):
+        test = LinkedList(0).addMany([1, 2, 3, 4, 5, 6, 7, 8, 9])
+        expected = LinkedList(0).addMany([1, 2, 3, 4, 6, 7, 8, 9])
+        removeKthNodeFromEnd(test, 5)
+        self.assertEqual(test.getNodesInArray(), expected.getNodesInArray())
+
+    def test_case_6(self):
+        test = LinkedList(0).addMany([1, 2, 3, 4, 5, 6, 7, 8, 9])
+        expected = LinkedList(0).addMany([1, 2, 3, 5, 6, 7, 8, 9])
+        removeKthNodeFromEnd(test, 6)
+        self.assertEqual(test.getNodesInArray(), expected.getNodesInArray())
+
+    def test_case_7(self):
+        test = LinkedList(0).addMany([1, 2, 3, 4, 5, 6, 7, 8, 9])
+        expected = LinkedList(0).addMany([1, 2, 4, 5, 6, 7, 8, 9])
+        removeKthNodeFromEnd(test, 7)
+        self.assertEqual(test.getNodesInArray(), expected.getNodesInArray())
+
+    def test_case_8(self):
+        test = LinkedList(0).addMany([1, 2, 3, 4, 5, 6, 7, 8, 9])
+        expected = LinkedList(0).addMany([1, 3, 4, 5, 6, 7, 8, 9])
+        removeKthNodeFromEnd(test, 8)
+        self.assertEqual(test.getNodesInArray(), expected.getNodesInArray())
+
+    def test_case_9(self):
+        test = LinkedList(0).addMany([1, 2, 3, 4, 5, 6, 7, 8, 9])
+        expected = LinkedList(0).addMany([2, 3, 4, 5, 6, 7, 8, 9])
+        removeKthNodeFromEnd(test, 9)
+        self.assertEqual(test.getNodesInArray(), expected.getNodesInArray())
+
+    def test_case_10(self):
+        test = LinkedList(0).addMany([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+        expected = LinkedList(0).addMany([1, 2, 3, 4, 5, 6, 7, 8, 9])
+        removeKthNodeFromEnd(test, 10)
+        self.assertEqual(test.getNodesInArray(), expected.getNodesInArray())
+
 if __name__ == '__main__':
     unittest.main()
