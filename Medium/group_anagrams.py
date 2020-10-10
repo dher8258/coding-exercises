1cdc28616ed5d6e9fcdefb1f76cfa6f447191e60
Daniel HG <danihergar@gmail.com>
2020-10-10 21:10:03 +0100

    feat: add unit tests to anagrams algorithm

--- Medium/group_anagrams.py
+++ Medium/group_anagrams.py
@@ -3,7 +3,7 @@ import unittest
 """
 The function takes in an array of strings and groups anagrams together.
 
-Anagrams are strings made up of exactly the same letters, where order doesn't
+Anagrams are strings made up of exactly the same letters, where order doesn't matter.
 For example, "cinema" and "iceman" are anagrams; similarly, "foo" and "ofo"
 are anagrams.
 
@@ -30,7 +30,26 @@ def groupAnagrams(words):
 
 class TestProgram(unittest.TestCase):
 	def test_case_1(self):
-		pass
+		words = ["yo", "act", "flop", "tac", "foo", "cat", "oy", "olfp"]
+		expected = [["yo", "oy"], ["flop", "olfp"], ["act", "tac", "cat"], ["foo"]]
+		output = list(map(lambda x: sorted(x), groupAnagrams(words)))
+
+		self.compare(expected, output)
+
+	def test_case_2(self):
+		words = ["melon", "avion", "hola", "zouk", "kouz", "loah", "onavi", "onmel", "lemon", "aloh"]
+		expected = [["melon", "lemon", "onmel"], ["avion", "onavi"], ["aloh", "hola", "loah"], ["kouz", "zouk"]]
+		output = list(map(lambda x: sorted(x), groupAnagrams(words)))
+
+		self.compare(expected, output)
+
+	def compare(self, expected, output):
+		if len(expected) == 0:
+			self.assertEqual(output, expected)
+			return
+		self.assertEqual(len(expected), len(output))
+		for group in expected:
+			self.assertTrue(sorted(group) in output)
 
 if __name__ == '__main__':
 	unittest.main()
